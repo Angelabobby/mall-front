a92cb4158ac750d0e75ba2e26ef52f349ad8b496
Angelabobby <qq673490419@163.com>
2022-12-10 21:44:26 +0800

    注册全局组件“三级联动”

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import TypeNav from "./pages/Home/TypeNav";
+// 注册全局组件
+Vue.component("TypeNav", TypeNav);
+
 Vue.config.productionTip = false;
 
 new Vue({
